80b3c964ac08f73d3ccf90b1b01e4d3962dd8f2c
nefelibatala1993 <leuela1993@gmail.com>
2022-06-02 12:24:35 -0700

    Added 5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point for my program
+ *
+ * Description - This program prints out desired output using printf
+ *
+ * Return: Zero when program success 
+ */
+
+int main()
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
