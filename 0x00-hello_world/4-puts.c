51c443f521ce1f33f20f26818be66535f0b06284
nefelibatala1993 <leuela1993@gmail.com>
2022-06-02 12:00:24 -0700

    Added 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
 
+/**
+ * main - the Entry Point of my program
+ *
+ * Description - used to print my desired strings using puts()
+ * Return: Zero when the program finishes succesfuly
+ */
+
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
