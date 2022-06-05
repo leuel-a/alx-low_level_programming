3cdca1b8226186f7e1ca0afc376a0b914005df8e
nefelibatala1993 <leuela1993@gmail.com>
2022-06-05 11:46:38 -0700

    Adde 9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of my program
+ *
+ * Description - This program list numbers
+ * Return: Zero upon program success
+ */
+
+int main(void)
+{
+	int n = 0;
+
+	while (n < 10)
+	{
+		if (n == 0)
+		{
+			goto here;
+		}
+		putchar(',');
+here:
+		putchar(' ');
+		putchar((n % 10) + '0');
+		n++;
+	}
+	putchar('\n');
+	return (0);
+}
