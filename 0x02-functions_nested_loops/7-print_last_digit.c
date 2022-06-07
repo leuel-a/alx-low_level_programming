85b07d5ce7dc728729491ea96d30ad6c9d5b5040
nefelibatala1993 <leuela1993@gmail.com>
2022-06-07 19:37:48 +0000

    Added 8-24_hours.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,4 +1,20 @@
 #include "main.h"
+#include <limits.h>
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int r;
+
+	r = print_last_digit(INT_MIN);
+	_putchar('0' + r);
+	_putchar('\n');
+	return (0);
+}
 
 /**
  * print_last_digit - This function prints the last digit of a number
---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * jack_bauer - Prints times in the song jack bauer
+ *
+ * Return: Zero upon program success
+ */
+
+void jack_bauer(void)
+{
+	int i, j, k, l;
+
+	for (i = 0; i < 3; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			if (i == 2 && j == 4)
+			{
+				break;
+			}
+			for (k = 0; k < 6; k++)
+			{
+				for (l = 0; l < 10; l++)
+				{
+					_putchar(i + '0');
+					_putchar(j + '0');
+					_putchar(':');
+					_putchar(k + '0');
+					_putchar(l + '0');
+					_putchar('\n');
+				}
+			}
+		}
+	}
+}
