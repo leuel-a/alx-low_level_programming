f49e7dd0be0fb78eb4fabaf904c17acf7d5d39f3
nefelibatala1993 <leuela1993@gmail.com>
2022-06-07 15:35:57 +0000

    Added 2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,18 +1,6 @@
 #include "main.h"
 
 /**
- * main - Entry point of my program
- *
- * Return: Zero upon program success
- */
-
-int main(void)
-{
-	print_alphabet_x10();
-	return (0);
-}
-
-/**
  * print_alphabet_x10 - prints lowercase alphabets 10x
  *
  * Description - This program prints all lowercase alphabet x10
