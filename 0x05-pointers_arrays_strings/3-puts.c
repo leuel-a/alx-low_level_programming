c9f1124969f4c64922ee7d785c4d1b89d62effae
nefelibatala1993 <leuela1993@gmail.com>
2022-06-13 18:54:58 +0000

    Added 3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _puts - This function prints a string
+ * @str: The string to be printed
+ *
+ * Return: This function returns nothing
+ */
+
+void _puts(char *str)
+{
+	int i = 0;
+
+	while (*(str + i) != '\0')
+	{
+		_putchar(*(str + i));
+		i++;
+	}
+}
