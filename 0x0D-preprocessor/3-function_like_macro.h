0daf2714f2044c5910299cb03b39bd66624eb3b3
nefelibatala1993 <leuela1993@gmail.com>
2022-07-04 18:03:51 +0000

    Added 3-function_like_macro.h

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,3 +1,6 @@
-#ifndef ABS(x)
+#ifndef FILE_ABS 
+#define FILE_ABS
+
 #define ABS(x) ((x) < 0 ? (-1) * (x) : (x))
+
 #endif
--- 0x0D-preprocessor/4-sum.h
+++ 0x0D-preprocessor/4-sum.h
@@ -1,3 +1,6 @@
-#ifndef SUM(x, y)
+#ifndef FILE_SUM
+#define FILE_SUM
+
 #define SUM(x, y) ((x) + (y))
+
 #endif
