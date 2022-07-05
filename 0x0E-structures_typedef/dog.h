528559281c367663e4ddeda2340f3938a89032da
nefelibatala1993 <leuela1993@gmail.com>
2022-07-05 00:36:19 +0000

    Added dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,14 @@
 #ifndef _DOG_H_
 #define _DOG_H_
 
+/**
+ * struct dog - This is a dog structure
+ * @name: Name of th dog
+ * @age: Age of the dog
+ * @owner: Owner of the dog
+ *
+ */
+
 struct dog
 {
 	char *name;
