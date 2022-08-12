2b92edf12bb7b428e5b3cf1d7134418b68e63e00
nefelibatala1993 <leuela1993@gmail.com>
2022-08-12 18:06:04 +0000

    Passed betty checks for 3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -12,13 +12,12 @@
  * positive value if s1 is greater than s2
  */
 
-int _strcmp(char *s1, char *s2 )
+int _strcmp(char *s1, char *s2)
 {
-    while ( *s1 && *s1 == *s2 )
-    {
-        ++s1;
-        ++s2;
-    }
-
-    return *s1 - *s2;
+	while (*s1 && *s1 == *s2)
+	{
+		++s1;
+		++s2;
+	}
+	return (*s1 - *s2);
 }
