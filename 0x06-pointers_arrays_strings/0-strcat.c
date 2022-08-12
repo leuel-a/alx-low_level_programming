8979dd6356a04ee7cb26350275e818edb0f148e6
nefelibatala1993 <leuela1993@gmail.com>
2022-08-12 07:32:49 +0000

    Added 1-strncat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -23,3 +23,22 @@ char *_strcat(char *dest, char *src)
 	dest[len + i] = '\0';
 	return (dest);
 }
+
+/**
+ * _strlen - Finds the length of the string
+ * @str: The string that you wish to find the length of
+ * @x: This will help us find the length of the string,
+ * and must be supplied by the user(it must all ways have
+ * a value of zero);
+ *
+ * Return: On success, it returns the length of the string
+ *
+ */
+
+int _strlen(char *str, int x)
+{
+        if (*str != '\0')
+                return (_strlen(str + 1, x + 1));
+        return (x);
+}
+
