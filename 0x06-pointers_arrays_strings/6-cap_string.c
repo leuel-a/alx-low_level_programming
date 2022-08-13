692c36e383a61215d1d469f5e0cbcb648a830c2d
nefelibatala1993 <leuela1993@gmail.com>
2022-08-13 21:42:38 +0000

    Added 6-cap_string.c

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ */
+
+char *cap_string(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] == ' ')
+		{
+			if (str[i + 1] >= 'a' && str[i + 1] <= 'z')
+			{
+				str[i + 1] -= 32;
+			}
+		}
+	}
+
+	return (str);
+}
