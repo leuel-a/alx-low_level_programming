fc9e0f86fe3babac5bc68f43395933b811efac64
nefelibatala1993 <leuela1993@gmail.com>
2022-08-20 14:25:40 +0000

    Made changes on 2-add_node.c

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -19,7 +19,10 @@ list_t *add_node(list_t **head, const char *str)
 		return (NULL);
 	temp->str = strdup(str);
 	if (temp->str == NULL)
+	{
+		free(temp);
 		return (NULL);
+	}
 	temp->len = strlen(temp->str);
 	temp->next = *head;
 	*head = temp;
