e7c64792baba6ba9c369c6e72c63aba8f85e5a5a
nefelibatala1993 <leuela1993@gmail.com>
2022-08-20 14:18:56 +0000

    Made some changes on 2-add_node.c

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -15,8 +15,8 @@ list_t *add_node(list_t **head, const char *str)
 {
 	list_t *temp = malloc(sizeof(list_t));
 
-	if(temp == NULL)
-		return NULL;
+	if (temp == NULL)
+		return (NULL);
 
 	temp->str = strdup(str);
 	temp->len = strlen(temp->str);
