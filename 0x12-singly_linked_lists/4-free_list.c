be61526bc3f24d74f6c9b1b40a0b849e867f36de
nefelibatala1993 <leuela1993@gmail.com>
2022-08-20 15:48:28 +0000

    Made some changes on 4-free_list.c

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -11,26 +11,24 @@
 
 void free_list(list_t *head)
 {
+	list_t *aux;
+	/**
+	 * aux - this will be an auxillary variable that will hold the address
+	 * of the next node in the list
+	 *
+	 * Description: aux is needed because if we free the node pointed by ptr
+	 * with out storing the address of the next node, there wont be any method
+	 * to retrieve the address of the next node. Hence, the linked list will
+	 * be lost
+	 *
+	 */
 	list_t *ptr = head;
 
-	while (ptr->next != NULL)
+	while (ptr)
 	{
-		list_t *aux = ptr;
-		/**
-		 * aux - is an auxilary variable that will help us free
-		 * the elements in the linked list.
-		 *
-		 * We can not use ptr to free the elements, because we
-		 * want it to iterate through the entire linked list
-		 *
-		 * If we freed ptr without storing it first i aux, then
-		 * we will lose the ability to traverse the list
-		 */
-
-		ptr = ptr->next;
-		free(aux);
+		aux = ptr->next;
+		free(ptr->str);
+		free(ptr);
+		ptr = aux;
 	}
-	head = NULL;
 }
-
-
