9205f2a95b4fff41485a1ed021633216357b4c88
nefelibatala1993 <leuela1993@gmail.com>
2022-08-24 17:24:49 +0000

    Passed betty in 6-pop_listint.c

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -1,6 +1,14 @@
 #include "lists.h"
 #include <stdlib.h>
 
+/**
+ * pop_listint - A function that deletes the first node
+ * in a linked list
+ * @head: A pointer to a pointer to the head
+ *
+ * Return: On success, it returns the data stored in the
+ * first node of the linked list
+ */
 int pop_listint(listint_t **head)
 {
 	int data;
