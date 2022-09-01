3c00aa61d74b9ed1fb4d74645632b8f6c98774e7
nefelibatala1993 <leuela1993@gmail.com>
2022-09-01 20:55:43 +0000

    Added 3-add_dnodeint_end.c

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -25,7 +25,7 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 		*head = temp;
 		return (*head);
 	}
-	while(aux->next != NULL)
+	while (aux->next != NULL)
 	{
 		aux = aux->next;
 	}
