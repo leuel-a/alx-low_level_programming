2befbfa69dedeeaaf16ea651b71586b1559c9aa1
nefelibatala1993 <leuela1993@gmail.com>
2022-09-01 22:49:48 +0000

    Added Files

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -1,62 +1,42 @@
-
-
 #include "lists.h"
-/**
-  * dlistint_len - returns number of elements in doubly linked list.
-  * @h: ponter to head node of list.
-  *
-  * Return: number of elements in list.
-  */
-size_t dlistint_len(const dlistint_t *h)
-{
-	/* i is counter var */
-	size_t i = 0;
-
-	while (h != NULL)
-	{
-		i++;
-		h = h->next;
-	}
-	return (i);
-}
 
 /**
-  * delete_nodeint_at_index - deletes node of a doubly linked list
-  * at a speified index.
-  * @head: pointer to head of d-list.
-  * @index: position of node to * 0 ;-)
-  *
-  * Return: 1 on success, -1 on failure.
-  */
-int delete_nodeint_at_index(dlistint_t **head, unsigned int index)
+ * delete_dnodeint_at_index - Deletes a node from a dlistint_t
+ *                            at a given index.
+ * @head: A pointer to the head of the dlistint_t.
+ * @index: The index of the node to delete.
+ *
+ * Return: Upon success - 1.
+ *         Otherwise - -1.
+ */
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-	unsigned int idx = 0;
-	dlistint_t *temp;
+	dlistint_t *tmp = *head;
 
-	temp = *head;
-	if (*head == NULL || index > dlistint_len(temp))
+	if (*head == NULL)
 		return (-1);
-	if (index == 0 && temp->next)
+
+	for (; index != 0; index--)
 	{
-		temp->next->prev = NULL;
-		*head = temp->next;
-		free(temp);
-		return (1);
+		if (tmp == NULL)
+			return (-1);
+		tmp = tmp->next;
 	}
-	else if (index == 0 && temp)
+
+	if (tmp == *head)
 	{
-		free(temp);
-		*head = NULL;
-		return (1);
+		*head = tmp->next;
+		if (*head != NULL)
+			(*head)->prev = NULL;
 	}
-	while (idx < index)
+
+	else
 	{
-		temp = temp->next;
-		idx++;
+		tmp->prev->next = tmp->next;
+		if (tmp->next != NULL)
+			tmp->next->prev = tmp->prev;
 	}
-	temp->prev->next = temp->next;
-	if (temp->next)
-		temp->next->prev = temp->prev;
-	free(temp);
+
+	free(tmp);
 	return (1);
 }
