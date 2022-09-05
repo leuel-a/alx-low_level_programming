a181a3efa4802abf182c84ed2e26c9bce47926b5
nefelibatala1993 <leuela1993@gmail.com>
2022-09-05 12:33:09 +0000

    Fixed error on 1-create_dynamic_lib.sh

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-gcc -Wall -Werror -Wextra -pendatic -std=gnu89 -fpic -c *.c
+gcc -Wall -Werror -Wextra -pedantic -std=gnu89 -fpic -c *.c
 gcc -Wall -Werror -Wextra -pedantic -std=gnu89 -fpic *.o -shared -o liball.so
