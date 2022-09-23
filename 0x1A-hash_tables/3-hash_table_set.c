d295dcfd055219bd487b0c15e65f4b25c9307fc9
nefelibatala1993 <leuela1993@gmail.com>
2022-09-23 09:44:46 +0300

    Passed betty for 3-hash_table_set.c

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,8 +1,13 @@
 #include "hash_tables.h"
 
 /**
+ * hash_table_set - this function adds an element to the hash table
+ * @ht: the hash table that a new eement will be added
+ * @key: the key for the new element
+ * @value: the value for the new element
  *
- *
+ * Return: On success, it returns 1. On error, it returns 0, and errno
+ * is set appropriately
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
@@ -14,8 +19,21 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 
 	hash_code = key_index((unsigned char *)key, ht->size);
 	new = malloc(sizeof(hash_node_t));
+	if (new == NULL)
+		return (0);
 	new->key = malloc(strlen(key) + 1);
+	if (new->key == NULL)
+	{
+		free(new);
+		return (0);
+	}
 	new->value = malloc(strlen(value) + 1);
+	if (new->value == NULL)
+	{
+		free(new->key);
+		free(new);
+		return (0);
+	}
 
 	strcpy(new->key, key);
 	strcpy(new->value, value);
