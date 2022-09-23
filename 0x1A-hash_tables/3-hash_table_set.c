64b3db9d874bfb954bc418f808dd9f23bd4273d6
nefelibatala1993 <leuela1993@gmail.com>
2022-09-23 10:17:00 +0300

    Attempted to pass checker for 3-hash_table_set.c

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -12,39 +12,68 @@
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int hash_code;
-	hash_node_t *new = NULL;
+	hash_node_t *new = NULL, *aux;
 
-	if (key == NULL)
+	if (!key)
 		return (0);
 
-	hash_code = key_index((unsigned char *)key, ht->size);
-	new = malloc(sizeof(hash_node_t));
+	new = create_node(key, value);
 	if (new == NULL)
 		return (0);
-	new->key = malloc(strlen(key) + 1);
-	if (new->key == NULL)
-	{
-		free(new);
-		return (0);
-	}
-	new->value = malloc(strlen(value) + 1);
-	if (new->value == NULL)
-	{
-		free(new->key);
-		free(new);
-		return (0);
-	}
-
-	strcpy(new->key, key);
-	strcpy(new->value, value);
-
+	hash_code = key_index((unsigned char *)key, ht->size);
 	if (ht->array[hash_code] == NULL)
 	{
 		ht->array[hash_code] = new;
 		new->next = NULL;
 		return (1);
 	}
+
+	aux = ht->array[hash_code];
+	while (aux != NULL)
+	{
+		if (strcmp(aux->key, key) == 0)
+		{
+			free(aux->value);
+			strcpy(aux->value, value);
+			return (1);
+		}
+		aux = aux->next;
+	}
+
 	new->next = ht->array[hash_code];
 	ht->array[hash_code] = new;
 	return (1);
+}
+
+/**
+ * create_node - this function creates a new node of type hash_node_t
+ * @key: this is the key of the new node
+ * @value: this is the value of the new node
+ *
+ * Return: On success, it returns a pointer to the newly created node.
+ * On error, it returns NULL.
+ */
+
+hash_node_t *create_node(const char *key, const char *value)
+{
+	hash_node_t *new;
+
+	new = malloc(sizeof(hash_node_t));
+	if (new == NULL)
+		return (NULL);
+
+	new->key = strdup(key);
+	if (new->key == NULL)
+	{
+		free(new);
+		return (NULL);
+	}
+	new->value = strdup(value);
+	if (new->value == NULL)
+	{
+		free(new->key);
+		free(new);
+		return (NULL);
+	}
+	return (new);
 }
