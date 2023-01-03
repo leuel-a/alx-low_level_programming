cdf3242c4ea3b73dd6fada8bc56e1e8ef321d495
nefelibatala1993 <leuela1993@gmail.com>
2023-01-03 04:28:15 +0300

    fix print of array -- spelling error

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -25,7 +25,7 @@ int binary_search(int *array, size_t size, int value)
 	left = 0, right = size - 1;
 	while (left <= right)
 	{
-		printf("Search in array: ");
+		printf("Searching in array: ");
 		for (i = left ; i <= right; i++)
 		{
 			if (check != 0)
