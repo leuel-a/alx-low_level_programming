be29701e05a6c02605230d67fb229978823bd66f
nefelibatala1993 <leuela1993@gmail.com>
2023-05-09 00:11:49 +0300

    passed betty and add edge case for 1-binary.c

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,11 +1,22 @@
 #include "search_algos.h"
 
 /**
+ * binary_search - searchs for an element in an array using
+ * the binary search algorithm.
+ * @size: size of the array to be searched
+ * @value: value to be searched in the array
+ * @array: the array to be searched
+ *
+ * Return: If the value found in the array, it returns the
+ * index of the place it was found in. Otherwise, it returns
+ * -1, and errno is set appropriately.
  */
 int binary_search(int *array, size_t size, int value)
 {
 	size_t low, high, mid;
 
+	if (!array)
+		return (-1);
 	low = 0, high = size - 1;
 	while (low <= high)
 	{
@@ -23,7 +34,13 @@ int binary_search(int *array, size_t size, int value)
 }
 
 /**
- * print_subarray
+ * print_subarray - prints the subarray that is being searched
+ * in the array
+ * @start: the start index of the subarray
+ * @end: the end index of the subarray
+ * @array: the array to be searched
+ *
+ * Return: Nothing.
  */
 void print_subarray(int *array, int start, int end)
 {
