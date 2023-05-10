67b26114d82112b8adb423ffaaf01529cf862dce
nefelibatala1993 <leuela1993@gmail.com>
2023-05-10 11:29:34 +0300

    add interpolation function declaration

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -8,5 +8,6 @@ int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 void print_subarray(int *array, int start, int end);
 int jump_search(int *array, size_t size, int value);
+int interpolation_search(int *array, size_t size, int value);
 
 #endif /* SEARCH_ALGOS_H  */
