1e90c745e6641e5458bc4aac624818576b27a882
nefelibatala1993 <leuela1993@gmail.com>
2023-05-10 12:06:28 +0300

    added check for NULL array

--- 0x1E-search_algorithms/102-interpolation.c
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -15,8 +15,9 @@ int interpolation_search(int *array, size_t size, int value)
 {
 	int low, high, mid;
 
+	if (!array)
+		return (-1);
 	low = 0, high = size - 1;
-
 	while (array[high] != array[low] && (value >= array[low]
 				&& value <= array[high]))
 	{
