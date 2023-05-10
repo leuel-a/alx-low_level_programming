341c0caabde3266ccc630793bd4dd1601a345b84
nefelibatala1993 <leuela1993@gmail.com>
2023-05-10 13:24:36 +0300

    added the exponential search algorithm

---
+++ 0x1E-search_algorithms/103-exponential.c
@@ -0,0 +1,52 @@
+#include "search_algos.h"
+
+/**
+ * exponential_search - search for a value in an array using the
+ * exponenetial search algorithm
+ *
+ * @array: the array to be searched
+ * @size: size of the array
+ * @value: value of the array
+ *
+ * Return: On success, it returns the first index of the value found
+ * in the array, otherwise it returns -1, and errno is set appropriately.
+ */
+int exponential_search(int *array, size_t size, int value)
+{
+	int jump, low, high, mid;
+
+	if (!array)
+		return (-1);
+
+	if (array[0] == value)
+		return (0);
+
+	jump = 1;
+	while (jump < (int)size && array[jump] <= value)
+	{
+		printf("Value checked array[%d] = [%d]\n", jump, array[jump]);
+		jump = jump * 2;
+	}
+
+	low = jump / 2;
+
+	if (jump >= (int)size)
+		high = size - 1;
+	else
+		high = jump;
+
+	printf("Value found between indexes [%d] and [%d]\n", low, high);
+
+	while (low <= high)
+	{
+		print_subarray(array, low, high);
+		mid = low + (high - low) / 2;
+		if (array[mid] == value)
+			return (mid);
+		else if (array[mid] < value)
+			low = mid + 1;
+		else
+			high = mid - 1;
+	}
+	return (-1);
+}
