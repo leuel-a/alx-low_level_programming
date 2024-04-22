ccaa9d511660ce3dfa4b38f6b8f618021c38e897
leuel-a <leuela1993@gmail.com>
2024-04-22 23:32:24 +0300

    Add solution to the last advanced task

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int count = 59;
+	char *str = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	write(STDERR_FILENO, str, count);
+	return (0);
+}
