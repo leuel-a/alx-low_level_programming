ae661498386e204ea63125465a657ac496bffdf2
leuel-a <leuela1993@gmail.com>
2024-04-25 12:26:58 +0300

    fix code

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -7,7 +7,7 @@
 */
 int main(void)
 {
-	int i, j, k, l;
+	int i, j, k, l, first = 0;
 
 	for (i = 0; i < 10; i++)
 	{
@@ -17,8 +17,17 @@ int main(void)
 			{
 				for (l = 0; l < 10; l++)
 				{
-					if (k <= i && l <= j)
+					if (k < i || l <= j)
 						continue;
+
+					if (first == 1)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+
+					if (first == 0)
+						first = 1;
 
 					putchar('0' + i);
 					putchar('0' + j);
@@ -27,12 +36,6 @@ int main(void)
 
 					putchar('0' + k);
 					putchar('0' + l);
-
-					if (k == 9 && l == 9)
-						continue;
-
-					putchar(',');
-					putchar(' ');
 				}
 			}
 		}
