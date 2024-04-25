fd8201c7948cd960f2cb437cb47171a8c13d591b
leuel-a <leuela1993@gmail.com>
2024-04-25 09:56:09 +0300

    add the solution to the third task

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 97; a < 123; a++)
+		putchar(a);
+	putchar('\n');
+	return (0);
+}
