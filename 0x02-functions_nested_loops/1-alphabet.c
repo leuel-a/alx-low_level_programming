437028d70967b6a9ff10f73bfa959cfcfed30ea0
leuel-a <leuela1993@gmail.com>
2024-04-29 13:14:00 +0300

    add the solution to the first task

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "_putchar.c"
+
+/**
+* print_alphabet - Prints the alphabet in lowercase
+ *
+ * Return: Always 0 (Success)
+*/
+void print_alphabet(void)
+{
+	int i = 97;
+
+	while (i < 123)
+		_putchar(i), i++;
+	_putchar('\n');
+}
