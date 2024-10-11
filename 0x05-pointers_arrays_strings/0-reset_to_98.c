690c2d6169c83bb6712fc357832c5756fd312b74
leuel-a <leuela1993@gmail.com>
2024-10-11 17:15:02 +0300

    add the first task

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - reset a point to a number to 98
+ *
+ * @n: pointer to the number
+ * Return: Nothing (Success)
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
